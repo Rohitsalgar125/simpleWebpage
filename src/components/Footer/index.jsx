1c2e6228f69a2aa7401fd30a267ff9ef728c312d
Rohitsalgar125 <rohitsalgar1255@gmail.com>
2024-01-07 23:24:32 +0530

    initial code

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white p-4 mt-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h5>Logistics</h5>
+            <p>&copy; 2024. All rights reserved.</p>
+          </div>
+          <div className="col-md-6 text-md-end">
+            <h5>Contact Us</h5>
+            <p>Email: info@example.com</p>
+            <p>Phone: 12345678</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
